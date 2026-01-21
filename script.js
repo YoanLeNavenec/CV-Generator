fb6cb880226c7903f70a0e62db32b9307a02e4eb
YoanLeNavenec <yoan.le-navenec@epitech.eu>
2026-01-21 16:02:05 +0100

    fixed script

--- script.js
+++ script.js
@@ -3,29 +3,30 @@ var down = document.getElementById("ExpPro");
 function AddJob() {
     var JobName = document.createElement("input");
     JobName.setAttribute("type", "text");
-    JobName.setAttribute("class", "WorkTitle")
+    JobDescription.classList.add("form-control-lg", "WorkTitle");
     JobName.setAttribute("placeholder", "Feral Pokemon");
 
     var JobPeriod = document.createElement("input");
     JobPeriod.setAttribute("type", "text");
-    JobPeriod.setAttribute("class", "WorkPeriod")
+    JobDescription.classList.add("form-control-lg", "WorkPeriod");
     JobPeriod.setAttribute("placeholder", "3th gen > 8th gen");
 
     var JobLocation = document.createElement("input");
     JobLocation.setAttribute("type", "text");
-    JobLocation.setAttribute("class", "WorkLocation")
+    JobLocation.classList.add("form-control-lg","WorkLocation");
     JobLocation.setAttribute("placeholder", "Road 6");
 
     var JobDescription = document.createElement("input");
     JobDescription.setAttribute("type", "text");
-    JobDescription.setAttribute("class", "WorkDescription")
+    JobDescription.classList.add("form-control-lg", "WorkDescription");
     JobDescription.setAttribute("placeholder", "Feral Pokemon to practice new trainers");
-
-    document.querySelector("#ExpPro").appendChild(JobName);
-    document.querySelector("#ExpPro").appendChild(JobPeriod);
-    document.querySelector("#ExpPro").appendChild(JobLocation);
-    document.querySelector("#ExpPro").appendChild(JobDescription);
-
+    
+    const exp_pro = document.querySelector("#ExpPro");
+    
+    exp_pro.appendChild(JobName);
+    exp_pro.appendChild(JobPeriod);
+    exp_pro.appendChild(JobLocation);
+    exp_pro.appendChild(JobDescription);
   }
 
   var down = document.getElementById("Diplos");
@@ -33,7 +34,7 @@ function AddJob() {
   function AddDiploma() {
     var DiploName = document.createElement("input");
     DiploName.setAttribute("type", "text");
-    DiploName.setAttribute("class", "Diploma")
+    DiploName.classList.add("form-control-lg", "Diploma");
     DiploName.setAttribute("placeholder", "Ribbon");
 
     document.querySelector("#Diplos").appendChild(DiploName);
@@ -44,7 +45,7 @@ function AddJob() {
   function AddSkill() {
     var SkillName = document.createElement("input");
     SkillName.setAttribute("type", "text");
-    SkillName.setAttribute("class", "Skill")
+    SkillName.classList.add("form-control-lg", "Skill");
     SkillName.setAttribute("placeholder", "Thunder Jaw");
 
     document.querySelector("#Skills").appendChild(SkillName);
@@ -55,7 +56,7 @@ function AddJob() {
   function AddSoftSkill() {
     var SoftSkillName = document.createElement("input");
     SoftSkillName.setAttribute("type", "text");
-    SoftSkillName.setAttribute("class", "SoftSkill")
+    SoftSkillName.classList.add("form-control-lg", "SoftSkill");
     SoftSkillName.setAttribute("placeholder", "Fast");
 
     document.querySelector("#SoftSkill").appendChild(SoftSkillName);
