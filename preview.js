6a00984ef5600b87ce3e5555d2357c4354325cf9
YoanLeNavenec <yoan.le-navenec@epitech.eu>
2026-01-23 17:15:38 +0100

    [ADD] added script for the header of the preview

---
+++ preview.js
@@ -0,0 +1,35 @@
+//Header script here//
+const InputName = document.querySelector('#Name');
+const PreviewName = document.querySelector('#Preview-Name');
+
+InputName.addEventListener('input', function(){
+  PreviewName.textContent = this.value;
+});
+
+const InputObjective = document.querySelector('#Objective');
+const PreviewObjective = document.querySelector('#Preview-Objective');
+
+InputObjective.addEventListener('input', function(){
+  PreviewObjective.textContent = this.value;
+});
+
+const InputEmail = document.querySelector('#Email');
+const PreviewEmail = document.querySelector('#Preview-Email');
+
+InputEmail.addEventListener('input', function(){
+  PreviewEmail.textContent = this.value;
+});
+
+const InputPhone = document.querySelector('#Phone');
+const PreviewPhone = document.querySelector('#Preview-Phone');
+
+InputPhone.addEventListener('input', function(){
+  PreviewPhone.textContent = this.value;
+});
+
+const InputAdress = document.querySelector('#Adress');
+const PreviewAdress = document.querySelector('#Preview-Adress');
+
+InputAdress.addEventListener('input', function(){
+  PreviewAdress.textContent = this.value;
+});
